982047116776d9865352ce6c8207dbc793eaaa94
Tim Lucas <t@toolmantim.com>
2017-07-12 19:47:48 +1000

    Fix rbenv detection

--- bash/rbenv.bash
+++ bash/rbenv.bash
@@ -2,7 +2,7 @@
 
 # Prioritize ~/.rbenv before homebrew version
 
-if [[ -d ~/.rbenv ]]; then
+if [[ -e ~/.rbenv/bin/rbenv ]]; then
   eval "$(~/.rbenv/bin/rbenv init -)"
 elif which rbenv > /dev/null 2>&1; then
   eval "$(rbenv init -)"
