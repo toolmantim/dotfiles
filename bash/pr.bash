2e061895fd7644adf1f989affa5ead62c22db0ae
Tim Lucas <t@toolmantim.com>
2018-08-07 14:27:08 +1000

    Fix the PR function to handle multiple remotes

--- bash/pr.bash
+++ bash/pr.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 pr () {
-  local repo=`git remote -v | grep -m 1 "(push)" | sed -e "s/.*github.com[:/]\(.*\)\.git.*/\1/"`
   local branch=`git name-rev --name-only HEAD`
+  local branch_origin=`git config --get "branch.${branch}.remote"`
+  local repo=`git remote get-url ${branch_origin} | sed -e "s/.*github.com[:/]\(.*\)\.git.*/\1/"`
   echo "... creating pull request for branch \"$branch\" in \"$repo\""
   open https://github.com/$repo/pull/new/$branch
 }
