a70a1cdb03588d8b7afa98c39fa41f8a34280d81
Tim Lucas <t@toolmantim.com>
2022-05-01 20:21:27 +1000

    Fix the PR command

--- bash/pr.bash
+++ bash/pr.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 pr () {
-  local branch=`git name-rev --name-only HEAD`
+  local branch=`git rev-parse --abbrev-ref HEAD`
   local branch_origin=`git config --get "branch.${branch}.remote"`
   local repo=`git remote get-url ${branch_origin} | sed -e "s/.*github.com[:/]\(.*\)\.git.*/\1/"`
-  echo "... creating pull request for branch \"$branch\" in \"$repo\""
   open https://github.com/$repo/pull/new/$branch
 }
